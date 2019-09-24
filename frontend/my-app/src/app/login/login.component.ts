109df755a12c2bff9d042ac14b83cfdf4560cfba
Matt Davis <mw.davis@hotmail.co.uk>
2019-09-24 22:48:55 +0100

    Login and register form are now functional. They have been styled using CSS, and perform POST requests to the backend API.

--- frontend/my-app/src/app/login/login.component.ts
+++ frontend/my-app/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email: string = "";
+  password: string = "";
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    console.log("Ran login function!!!!!");
+    console.log(this.email + this.password);
+    this.http.post("http://localhost:4000/login",
+    {
+    "email": {"data": this.email, "id": "email"},
+    "password": {"data": this.password, "id": "password"}
+    })
+    .subscribe(
+      data  => {
+        console.log("POST Request is successful ", data);
+      },
+      error  => {
+        console.log("Error", error);
+      }
+
+    );
+    this.http.get("http://localhost:4000/")
+    .subscribe(
+      data  => {
+        console.log("GET Request is successful ", data);
+      },
+      error  => {
+        console.log("Error", error);
+      }
+
+    );
   }
 
 }
--- frontend/my-app/src/app/register/register.component.ts
+++ frontend/my-app/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  email: string = "";
+  firstName: string = "";
+  lastName: string = "";
+  password: string = "";
+  passwordRepeat: string = "";
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  register() {
+    console.log("Register function ran!");
+    this.http.post("http://localhost:4000/register",
+    {
+    "email": {"data": this.email, "id": "email"},
+    "firstName": {"data": this.firstName, "id": "firstName"},
+    "lastName": {"data": this.lastName, "id": "lastName"},
+    "password": {"data": this.password, "id": "password"},
+    "passwordRepeat": {"data": this.passwordRepeat, "id": "passwordRepeat"},
+    })
+    .subscribe(
+      data  => {
+        console.log("POST Request is successful ", data);
+      },
+      error  => {
+        console.log("Error", error);
+      }
+
+    );
   }
 
 }
