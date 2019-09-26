6d0884553973211132ec93ceea950705de47492a
Matt Davis <mw.davis@hotmail.co.uk>
2019-09-26 19:57:55 +0100

    Implemented Angular proxy to backend so that API calls are the same and cookie are preserved. CORS is nolonger required in any case as development environments should appear to originate from the same host.

--- backend/my-app/app.js
+++ backend/my-app/app.js
@@ -18,9 +18,6 @@ var date = new Date();
 // Parse body of request as a JSON object.
 app.use(express.json());
 
-// ONLY TO BE USED IN DEVELOPMENT Allow requests from different origins.
-//var cors = require('cors');
-//app.use(cors());
 
 var sessionStore = new MySQLStore({}, con);
 
@@ -142,88 +139,7 @@ try {
     }
 
 
-    /////////////////////// Validations ////////////////////////
-    class Validation {
-        constructor() {
-            this.output = {};
-            this.output.success = true;
-            this.output.errors = [];
-        }
-        
-        /**
-         * Email validation function. Checks email is 100 character or less and is a valid email.
-         * If not it will add an error report to the Validation object.
-         * @param {*} email The email object to be validated. Should contain the email at email.data and optional id at email.id
-         */
-        email(email) {
-            var regex = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/);
-            if (this.defined(email))
-            {
-                if (email.data.length > 100) {
-                    this.handleFail(email, "Email is over 100 characters.");
-                }
-                if (!regex.test(email.data)) {
-                    this.handleFail(email, "Email is not valid.");
-                }
-            }
-            else
-            {
-                this.handleFail(email, "Please enter your email.");
-            }
-        }
 
-        name(name) {
-            if (this.defined(name))
-            {
-                if (name.data.length > 50) {
-                    this.handleFail(name, "Name is over 50 characters.");
-                }
-                if (name.data.length <=0) {
-                    this.handleFail(name, "Please enter your name.");
-                }
-            }
-            else
-            {
-                this.handleFail(name, "Please enter your name.");
-            }
-        }
-
-        password(pass, passRepeat) {
-            if (this.defined(pass) && this.defined(passRepeat))
-            {
-                if (pass.data.length < 8) {
-                    this.handleFail(pass, "Password must be 8+ characters.");
-                }
-                if (pass.data !== passRepeat.data) {
-                    this.handleFail(pass, "Passwords do not match.");
-                }
-            }
-            else
-            {
-                this.handleFail(pass, "Please enter a password.");
-            }
-        }
-
-        defined(variable) {
-            if (typeof variable !== 'undefined')
-                return true;
-            else
-                return false;
-        }
-
-        handleFail(data, reason) {
-            this.output.success = false;
-            this.output.errors.push({"id": data.id, "reason": reason});
-        }
-
-        getOutput() {
-            return this.output;
-        }
-
-        sendResponse(res) {
-            res.send(JSON.stringify(this.output));
-        }
-    }
 
 }
 catch(err)
--- frontend/my-app/src/app/backendURL-template.ts
+++ frontend/my-app/src/app/backendURL-template.ts
@@ -1,2 +1,2 @@
-// Backend URL, http://localhost:4000 for dev, https://auth.mattdavis.info/api for prod.
-export default "http://localhost:4000";
+// Backend URL, http://localhost:4200/api for dev, https://auth.mattdavis.info/api for prod.
+export default "http://localhost:4200/api";
