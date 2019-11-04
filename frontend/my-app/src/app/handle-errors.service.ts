df4ae7e8f5f6359670b2877d0285e9b0624cd329
Matt Davis <mw.davis@hotmail.co.uk>
2019-11-04 17:02:09 +0000

    Added a removeErrors function to the handle-errors service. Allows the error outline to be removed from an array of HTML element IDs.

--- frontend/my-app/src/app/handle-errors.service.ts
+++ frontend/my-app/src/app/handle-errors.service.ts
@@ -40,6 +40,20 @@ export class HandleErrorsService {
     this.showNotification(errorMessage);
   }
 
+  /**
+   * Removes the error outline from an array of fields.
+   * Field names should be given in ["email", "firstName"] form
+   * where their IDs are actually #email, #firstName.
+   * @param el The ElementRef of your route.
+   * @param fields The fields you wish to remove an outline from.
+   */
+  removeErrors(el, fields) {
+    for (let j = 0; j < fields.length; j++) {
+      if (fields[j] !== null || fields[j] !== "all" || fields[j] !== "fatal")
+          el.nativeElement.querySelector("#"+fields[j]).classList.remove("input-error");
+    }
+  }
+
   showNotification(text) {
     this.notificationValue = text;
     this.notificationHidden = false;
